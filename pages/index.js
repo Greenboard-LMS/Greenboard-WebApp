c3ccc2e3eff93a696b612946e1f333073821f2c9
Varun <31084854+varunsingh87@users.noreply.github.com>
2021-12-31 14:34:19 -0500

    Add "Follow for updates" card

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image';
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -28,15 +27,26 @@ export default function Home() {
         <img width="128" height="96" src="/favicon/favicon.png" />
 
         <div className={styles.grid}>
-          <a href="https://github.com/Greenboard-LMS/Greenboard-WebApp" className={styles.card}>
+          <a target="_blank" rel="noreferrer noopener" href="https://github.com/Greenboard-LMS/Greenboard-WebApp" className={styles.card}>
             <h3>Open Source &rarr;</h3>
             <p>Be one of the first to help with the early development stages on GitHub</p>
+          </a>
+          <a target="_blank" rel="noreferrer noopener" href="https://twitter.com/GreenboardLMS" className={styles.card}>
+            <h3>Follow for updates</h3>
+            <p>Stay up to date with the latest features and announcements</p>
           </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <p>All rights reserved &copy; 2021 Borum Tech</p>
+        <div className="copyright">
+          <p>&copy; 2021-{new Date().getFullYear()} Borum Tech. All rights reserved.</p>
+        </div>
+        {/* <div className={styles.socialIcons}>
+          <SocialIcon target="_blank" rel="noreferrer noopener" url="https://twitter.com/GreenboardLMS" />
+          <SocialIcon target="_blank" rel="noreferrer noopener" url="https://github.com/Greenboard-LMS/Greenboard-WebApp" />
+        </div> */}
+
       </footer>
     </div>
   )
